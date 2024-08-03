0043856742df69b13fac163a8aa76cfa34c5a7be
niklas-laptop <niklas.bognar@gmail.com>
2024-08-03 18:15:07 +0100

    3rd stage

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -103,9 +103,13 @@ func main() {
 		case LEFT_PAREN:
 			fmt.Println("LEFT_PAREN ( null")
 		case RIGHT_PAREN:
-
 			fmt.Println("RIGHT_PAREN ) null")
+		case LEFT_BRACE:
+			fmt.Println("LEFT_BRACE { null")
+		case RIGHT_BRACE:
+			fmt.Println("RIGHT_BRACE } null")
 		}
+
 	}
 	fmt.Println("EOF  null")
 }

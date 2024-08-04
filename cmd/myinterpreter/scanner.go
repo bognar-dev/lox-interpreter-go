2a5640f5e5d5a0b40a81b548408428736cc5b159
niklas-laptop <niklas.bognar@gmail.com>
2024-08-04 17:54:38 +0100

    5th stage

--- cmd/myinterpreter/scanner.go
+++ cmd/myinterpreter/scanner.go
@@ -58,8 +58,7 @@ const (
 	VAR    TokenType = "var"
 	WHILE  TokenType = "while"
 
-	EOF     TokenType = "null"
-	UNKNOWN TokenType = "unknown"
+	EOF TokenType = "null"
 )
 
 func (t TokenType) toString() string {
@@ -199,15 +198,37 @@ func (s *Scanner) printTokens(tokens []Token) {
 
 func (s *Scanner) scanToken() {
 	c := s.advance()
-	if c != UNKNOWN {
-		s.addToken(c)
-		return
-	}
+	switch c {
+	case LEFT_PAREN:
+		s.addToken(LEFT_PAREN)
+	case RIGHT_PAREN:
+		s.addToken(RIGHT_PAREN)
+	case LEFT_BRACE:
+		s.addToken(LEFT_BRACE)
+	case RIGHT_BRACE:
+		s.addToken(RIGHT_BRACE)
+	case STAR:
+		s.addToken(STAR)
+	case DOT:
+		s.addToken(DOT)
+	case COMMA:
+		s.addToken(COMMA)
+	case PLUS:
+		s.addToken(PLUS)
+	case MINUS:
+		s.addToken(MINUS)
+	case SEMICOLON:
+		s.addToken(SEMICOLON)
+	case SLASH:
+		s.addToken(SLASH)
+	case EOF:
+		s.addToken(EOF)
+	default:
+		if s.errorMsg == nil {
+			s.errorMsg = fmt.Errorf("[line " + strconv.Itoa(s.line) + "] Error: Unexpected character: " + s.peek().toString())
+		} else {
+			s.errorMsg = fmt.Errorf("%w \n %s", s.errorMsg, "[line "+strconv.Itoa(s.line)+"] Error: Unexpected character: "+s.peek().toString())
+		}
 
-	if s.errorMsg == nil {
-		s.errorMsg = fmt.Errorf("[line " + strconv.Itoa(s.line) + "] Error: Unexpected character: " + s.peek().toString())
-	} else {
-		s.errorMsg = fmt.Errorf("%w \n %s", s.errorMsg, "[line "+strconv.Itoa(s.line)+"] Error: Unexpected character: "+s.peek().toString())
 	}
-
 }
